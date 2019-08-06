f4e3d8901a10e3e583bdb1ec7fe50f0b069b8f55
Samy Amraoui <sametzy@gmail.com>
2019-08-06 22:37:47 +0200

    Improve Problem 9 solution

--- src/9.js
+++ src/9.js
@@ -21,9 +21,32 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    const reverse = String(x).split('').reverse().join('');
-    return x == reverse;
+var isPalindrome = function (x) {
+    if (x >= 0) {
+        // Find highest 10 divisor of x
+        let divisor = 1;
+        while (x / divisor >= 10) {
+            divisor *= 10;
+        }
+
+        while (x !== 0) {
+            const leading = Math.floor(x / divisor);
+            const trailing = x % 10;
+
+            // Check if leading and trailing digits are identical
+            if (leading !== trailing) {
+                return false;
+            }
+
+            // Removing leading and trailing digits
+            x = Math.floor((x % divisor) / 10);
+            divisor /= 100;
+        }
+
+        return true;
+    } else {
+        return false;
+    }
 };
 
 module.exports = { isPalindrome };
